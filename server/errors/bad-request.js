ce99f9993fe902d018200c37d5da9d79344e8c29
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-05-30 09:52:01 +0200

    modifiyng bad-request.js

--- server/errors/bad-request.js
+++ server/errors/bad-request.js
@@ -1,5 +1,5 @@
 const CustomAPIError = require('./customError');
-const { StatusCodes, METHOD_FAILURE } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
 class BadRequestError extends CustomAPIError {
   constructor(message) {
